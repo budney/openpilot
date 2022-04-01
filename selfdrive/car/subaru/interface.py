2275abd8c61185b5efb5da338030da4eca0e271c
Len Budney <len.budney@gmail.com>
2022-04-01 10:37:35 -0400

    Merged tuning for Crosstrek/Impreza at higher torque. Taken from michaelhonan's repository, commit ID 5efe79fc696abdc7cf271459c962095922bc56a1.

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -46,14 +46,15 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.0025, 0.1], [0.00025, 0.01]]
 
     if candidate == CAR.IMPREZA:
+      ret.safetyConfigs[0].safetyParam = 2  # increase limit on some crosstrek/impreza
       ret.mass = 1568. + STD_CARGO_KG
       ret.wheelbase = 2.67
       ret.centerToFront = ret.wheelbase * 0.5
-      ret.steerRatio = 15
-      ret.steerActuatorDelay = 0.4   # end-to-end angle controller
-      ret.lateralTuning.pid.kf = 0.00003333
+      ret.steerRatio = 13
+      ret.steerActuatorDelay = 0.18   # end-to-end angle controller
+      ret.lateralTuning.pid.kf = 0.00005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.133, 0.2], [0.0133, 0.02]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.1, 0.2], [0.01, 0.02]]
 
     if candidate == CAR.IMPREZA_2020:
       ret.mass = 1480. + STD_CARGO_KG
